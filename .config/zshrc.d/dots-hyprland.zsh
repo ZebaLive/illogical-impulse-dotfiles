04ab11a23bd7be8490cabf9cdb7519f04b038563
ZebaLive <paulius.pluta@gmail.com>
2026-02-28 15:38:04 +0200

    Add modified kitty and starship configs

--- .config/zshrc.d/dots-hyprland.zsh
+++ .config/zshrc.d/dots-hyprland.zsh
@@ -1,5 +1,5 @@
 # Use the generated color scheme
 
 if [ -z "$VSCODE_INJECTION" ] && [ -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt ]; then
-    cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    command cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
 fi
